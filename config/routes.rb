351be095311a181aaf4ecd33f2b35ea33fce9d38
Brittanie Pham <brittaniepham@gmail.com>
2018-11-08 16:36:10 -0700

    sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-root 'static#home'
-get '/about', to: 'static#about'
-resources :articles
+  root 'static#home'
+  get '/about', to: 'static#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
