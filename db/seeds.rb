673a6caa81ea315f0b971ff140fa712c4448e620
Brittanie Pham <brittaniepham@gmail.com>
2018-11-08 23:33:46 -0700

    clean up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
- 10.times do |i|
+10.times do |i|
   email = "test#{i}@test.com"
   username = "Brittanie#{i}"
   password = "password"
-
   user = User.create(email: email, username: username, password: password)
 
   10.times do |j|
-    user.articles.create(title: "This is the title of my article #{j}", description: "This is the description of my article #{j}")
+    title = "This is the title of my article #{j}"
+    description = "This is the description of my article #{j}"
+    user.articles.create(title: title, description: description)
   end
- end
+end
 
- puts "seeded"
+puts "seeded yo"
