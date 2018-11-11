ab0f8c045818ee31daafbea5b60b0bbeb32bf695
Brittanie Pham <brittaniepham@gmail.com>
2018-11-11 16:34:06 -0700

    Categories Controller and Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root 'static#home'
   get '/about', to: 'static#about'
+
   resources :articles
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 end
