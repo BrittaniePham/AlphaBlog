6894a1d3fa74dd96aa4510d07ac38b2a7193b252
Brittanie Pham <brittaniepham@gmail.com>
2018-11-15 19:04:54 -0700

    create categories test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,12 +8,28 @@ class CategoriesController < ApplicationController
   end
   
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:success] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   private
   
   def set_category
     @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
